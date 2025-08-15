b17578a2b62e7d81726214d6a1a29c17b5b01050
outslept <135520429+outslept@users.noreply.github.com>
2025-08-15 08:29:34 +0300

    chore: comment old algo, to only run the new one

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import { runDependencyAnalysis as runOldAnalysis } from './src/analyze/dependencies_old.js';
-import { runDependencyAnalysis as runNewAnalysis } from './src/analyze/dependencies_new.js';
+import { runDependencyAnalysis as runNewAnalysis } from './src/analyze/dependencies_one_more_time.js';
 import { LocalFileSystem } from './src/local-file-system.js';
 
 async function run() {
   const fileSystem = new LocalFileSystem(process.cwd());
 
-  console.log('Running old algorithm...');
-  const oldResult = await runOldAnalysis(fileSystem);
-  console.log('Old result:', oldResult.stats);
+  // console.log('Running old algorithm...');
+  // const oldResult = await runOldAnalysis(fileSystem);
+  // console.log('Old result:', oldResult.stats);
 
   console.log('\nRunning new algorithm...');
   const newResult = await runNewAnalysis(fileSystem);
